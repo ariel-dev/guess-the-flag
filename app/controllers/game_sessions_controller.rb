63e6a79c281d6109771c0eb5cb8df5119b14090c
Ariel German <dev@arielgerman.com>
2024-12-29 02:36:07 -0500

    fix a Zeitwerk issue

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,7 +1,9 @@
-def start
-  # ... existing start game logic ...
-  
-  ActionCable.server.broadcast "game_session_#{@game_session.session_code}",
-    event: 'game_started',
-    data: { session_code: @game_session.session_code }
+class GameSessionsController < ApplicationController
+  def start
+    # ... existing start game logic ...
+    
+    ActionCable.server.broadcast "game_session_#{@game_session.session_code}",
+      event: 'game_started',
+      data: { session_code: @game_session.session_code }
+  end 
 end 
